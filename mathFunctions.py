2006aa36dcb58bb84825c3ec490fba927021923b
Shrey Chang <shreycha.61406@redlandsschools.net>
2018-09-11 16:18:04 +0000

    Math

---
+++ mathFunctions.py
@@ -0,0 +1,4 @@
+"""
+write a program that will take the user input and return that input times 3 
+Make it work for decimals ad integers
+"""mathFunctions.py
---
+++ timesThree.py
@@ -0,0 +1,7 @@
+"""
+write a new program that will take theuser input and return that input times 3 
+Make it work for decimals or integers
+"""
+
+x= float(input('Please enter a number: '))
+print("The number " + str(x) + "times 3 is equal to: " + str(x*3))
