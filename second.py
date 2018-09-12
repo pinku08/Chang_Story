02a4a8da81dd9999926000bb3d519935548bf0ca
Shrey Chang <shreycha.61406@redlandsschools.net>
2018-09-12 03:33:33 +0000

    age at 18

---
+++ second.py
@@ -0,0 +1,15 @@
+"""
+Triple QUOTES will allow mw to comment out multiple lines.Triple
+
+Write a program taht will ask the user what their age is and then,
+determine if they are old enough to vote or not and respond appropriately.determine
+
+"""
+
+age=input("How old are you?")
+
+if age < 18:
+    print ("You're too young to vote. Please wait a few years.")
+    
+else:
+    print("You are old enough to vote. Who are you voting for?")
