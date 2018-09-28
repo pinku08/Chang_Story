5f8c29473a271e03dacec6d18adcf00140a27804
Shrey Chang <shreycha.61406@redlandsschools.net>
2018-09-28 14:56:19 +0000

    exit

---
+++ addsix-pop quiz.py
@@ -0,0 +1,7 @@
+
+
+def (addSix):
+    x input= ('What is the number added to 6?')
+    print("x+6")
+    return=x+6
+    
---
+++ story1.py
@@ -0,0 +1,14 @@
+ask=input("Do you accept this mission?\'yes or no\' ")
+if ask == 'yes':
+    print("you mission is to save a dog from a well ")
+else:
+    print("go get some dinner ")
+
+ask = 'yes'
+print("go get supplies to save dog ")
+
+x=input("What do you get a bucket or a firefighter?\'bucket or firefigter\'")
+if x =="bucket"
+    print ("what type of transportation?")
+else:
+    sys.exit()
