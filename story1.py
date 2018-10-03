e8ac18bd4a724bf219024a6642ca53afd1cd4e94
Shrey Chang <shreycha.61406@redlandsschools.net>
2018-10-03 15:26:23 +0000

    trying to make the no section work

--- story1.py
+++ story1.py
@@ -1,14 +1,60 @@
-ask=input("Do you accept this mission?\'yes or no\' ")
-if ask == 'yes':
-    print("you mission is to save a dog from a well ")
-else:
-    print("go get some dinner ")
+def ask():
+    ask=input("Do you accept this mission?\'type yes or no\' ")
+    if (ask == "yes" or ask == "Yes" ) :
+        print("Your mission is to save a dog from a well. ")
+        print("Go get supplies to save dog ")
+        def choice1():
+            x=input("What do you get a bucket and rope or a firefighter?\'type bucket and rope or firefighter\' ")
+            if (x == "bucket and rope"):
+                print ("What type of transportation?")
+            elif (x == "firefighter"):
+                exit("You failed. It is the fire season, they are busy. ")
+            else:
+                print("You can't type. You failed ")
+        def transportation():
+            t=input("What transportation do you want?\'type taxi or bus\' ")
+            if (t == 'bus'):
+                print("What type of knots?")
+            else:
+                exit("You failed. It is dirty you got sick and died ")
+ask()
 
-ask = 'yes'
-print("go get supplies to save dog ")
+choice1()
+ 
+transportation()
 
-x=input("What do you get a bucket or a firefighter?\'bucket or firefigter\'")
-if x =="bucket"
-    print ("what type of transportation?")
-else:
-    sys.exit()
+bus()
+elif (ask == "no" or ask == "No" ) :
+    print("Go get some dinner. ")
+        
+
+    
+"""def bus():
+    b=input("What knots?")
+    if (b == "double hitch"):
+        print("You saved the dog. You win!!!")
+    else:
+        exit("You failed. It is not strong enough.")"""
+        
+
+ask()
+
+choice1()
+ 
+transportation()
+
+bus()
+
+
+
+print("go get something to eat ")
+def Eat():
+    a=input("chipotle or pizza hut\'type pizza hut or chipotle\'")
+    if ask==("pizza hut"):
+        print("What types of flavor do you want?\'meat lover or cheese\'")
+    else:
+        print('You got diarrea and failed.')
+    
+
+
+        
