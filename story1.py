b0135f96c40d86e8494ab3a5c55b2ccdafb48cd8
Shrey Chang <shreycha.61406@redlandsschools.net>
2018-10-08 04:50:40 +0000

    colors and styling

--- story1.py
+++ story1.py
@@ -9,60 +9,60 @@ def ask():#this is choice number 1 of whether uou want to do the mission of savi
     if (ask == "yes" or ask == "Yes" ) :
         ask = True
         print(Back.CYAN + "Your mission is to save a dog from a well." + Back.RESET)
-        print("Go find supplies to save dog ")
+        print(Fore.RED + "Go find supplies to save dog. " +Fore.RESET)
     elif(ask == "no" or ask == "No" ):
         ask = False
-        print("Go get some dinner. ")
+        print(Back.CYAN + "Go get some dinner. " + Back.RESET)
     return ask
     
-def choice1():
-    x=str(input("What do you get a bucket and rope or a firefighter? \'type bucket and rope or firefighter:\' "))
+def choice1(): #This asks what should you do if you wanted to save the dog.
+    x=str(input("What do you get " + Back.GREEN+ "a bucket and rope" + Back.RESET + " or " + Back.GREEN + "a firefighter" + Back.RESET + "?\'type bucket and rope or firefighter:\' "))
     if (x == "bucket and rope"):
-        print ("What type of transportation?")
+        print ("What type of" + Back.BLACK +Fore.YELLOW + " transportation" + Back.RESET + Fore.RESET + "?")
     elif (x == "firefighter"):
-        exit("You failed. It is the fire season, they are busy. ")
+        exit(Fore.RED + "You failed. It is the fire season, they are busy." + Fore.RESET)
     else:
         print("You can't type. You failed ")
     return x    
     
 def transportation():#who want to take a taxi 
-    t=str(input("What transportation do you want?\'type taxi or bus:\' "))
+    t=str(input("What"+ Back.BLACK + Fore.YELLOW + "transportation" + Back.RESET + Fore.RESET + " do you want?\'type taxi or bus:\' "))
     if (t == 'bus'):
         print("What type of knots?")
     else:
-        exit("You failed. It is dirty you got sick and died ")
+        exit(Fore.RED + "You failed. It is dirty you got sick and died." + Fore.RESET)
     return t
     
-def bus():#i dont even know what a double hitch is
-    b=str(input("Double hitch or double knot: \'type Double hitch or double knot\'"))
+def bus():#What type of knot in neccessary and best fit to save the dog.
+    b=str(input(Fore.CYAN+ "How would you tie the knot, Double hitch or double knot? \'type Double hitch or double knot\'" + Fore.RESET))
     if (b == "Double hitch"):
-        print("You saved the dog. You win!!!")
+        print(Fore.GREEN + "You saved the dog. You win!!!" + Fore.RESET)
     else:
-        exit("You failed. It is not strong enough.")
+        exit(Fore.RED +"You failed. It is not strong enough."+ Fore.RESET)
     return b
 
-def eat():#every hour goes by you get a hour closer to your next pizza.... This code is asking them what they want to eat :)
-    a=str(input("Chipotle or Pizza Hut? \'type pizza hut or chipotle\' "))
+def eat():#Every hour goes by you get a hour closer to your next pizza.... This code is asking them what they want to eat :)
+    a=str(input(Fore.CYAN + "Where would you go to eat," + Fore.RESET + Fore.GREEN + "Chipotle or Pizza Hut" + Fore.RESET + Fore.CYAN + "? \'type pizza hut or chipotle\'" + Fore.RESET))
     if a==("pizza hut"):
-        print("What types of flavor do you want? \'meat lover or cheese\'  ")
+        print("What types of " +Back.WHITE +Fore.GREEN + "topping" +Back.RESET +Fore.RESET+ " do you want? \'meat lover or cheese\'  ")
     else:
-        print('You got diarrhea and failed.')
+        print(Back.WHITE+ Fore.GREEN+ 'You got diarrhea and failed.'+Back.RESET + Fore.RESET)
         return a
 
-def dog():
-    bog=str(input("(hint you are lactose intolerance) "))
-    if bog==("meat lover"):
-        print("Good pick let's go get a dog.")
+def dog():#This asks what topping you would get for your pizza.
+    dog=str(input(Fore.RED + "(Hint: You are lactose intolerance.) " + Fore.RESET))
+    if dog==("meat lover"):
+        print("Good pick." + Fore.YELLOW +"Let's go get a dog." + Fore.RESET)
     else:
-        print("You failed :(")
-        return bog
+        print(Fore.RED + "You failed. :(" + Fore.RESET)
+        return dog
 
-def store():
-    p=str(input("Where do you want to go petco or a sketchy van\' type petco or van \' "))
+def store():#This questions you on What place you would go to get a new dog.
+    p=str(input("Where do you want to go," + Back.WHITE + Fore.BLUE + "Petco or a sketchy van" + Back.RESET + Fore.RESET + "? \'type petco or van \' "))
     if p==("petco"):
-        print("you get a normal dog yay!")
+        print(Fore.GREEN + "You got a new normal dog, yay!" + Fore.RESET)
     elif p==("van"):
-        print("You get a dog with super powers YAY! ")
+        print(Fore.YELLOW + "You got a new super power dog!!! THAT'S SUPER!" + Fore.RESET)
         return p
 
 ask  = ask()
